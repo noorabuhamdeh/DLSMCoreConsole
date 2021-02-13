f310dc156cc6f16eb1012ca8ad9d0a65d0fd19fa
Noor Abuhamdeh <NoorAbuhamdeh@hotmail.com>
2021-02-13 15:37:05 +0200

    Add testing controller to start test from swagger

---
+++ WebServer/Controllers/MeterTestingController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServer.database;
+using WebServer.MeterIntegration;
+
+namespace WebServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class MeterTestingController : Controller
+    {
+        private readonly ReadersManager readersManager;
+        private readonly ILogger<MeterTestingController> logger;
+        private readonly DatabaseContextEF databaseContext;
+
+        public MeterTestingController(ReadersManager _readersManager, ILogger<MeterTestingController> _logger, DatabaseContextEF _databaseContext)
+        {
+            readersManager = _readersManager;
+            logger = _logger;
+            databaseContext = _databaseContext;
+        }
+       
+        [HttpGet]
+        public async Task<IActionResult> GetMeterIntialization(int meterId)
+        {
+            try
+            {
+                var meter = databaseContext.Meters.SingleOrDefault(s => s.Id == meterId);
+                var port = databaseContext.ComPortMedias.SingleOrDefault(s => s.MeterId == meterId);
+                var mappings = databaseContext.MeterMappings.Where(w => w.MeterId == meterId).ToList();
+
+                if (meter == null)
+                    return NotFound("Meter");
+                if (port == null)
+                    return NotFound("Port");
+                if (mappings == null)
+                    return NotFound("Meter mappings");
+
+                await readersManager.InitializeComPortMeter(meter, port, mappings);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Json(ex);
+            }
+        }
+    }
+}
